daa2502b0539adc23002281a29184aca16985949
Konain1 <kaunainkasab@gmail.com>
2024-08-10 17:01:21 +0530

    profile

--- frontend/votingFrontend/src/components/Dashboard.jsx
+++ frontend/votingFrontend/src/components/Dashboard.jsx
@@ -2,16 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import DashNav from './DashNav';
-import { LineChart, Line } from 'recharts';
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
 import Chart from './charts/Chart';
-
+import Profile from './Profile';
 function Dashboard() {
   const [user, setUser] = useState(null);
   const [error, setError] = useState('');
   const navigate = useNavigate();
-
-
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -37,78 +33,74 @@ function Dashboard() {
   }, [navigate]);
 
   return (
-    <div className=''>
+    <>
+    <div>
     
       {error && <p className='text-red-500'>{error}</p>}
       {user  ? (
-        <div className='bg-[#0c0601] px-0 h-[70vh] sm:h-[55vh] text-white relative'>
-          {/* <h1 className="text-[30px] p-2">Welcome , {user.name}</h1> */}
+        <div className='bg-[#0c0601] px-0 h-[70vh] sm:h-[55vh] text-white relative hidden sm:block'>
+          {/* User Greeting Section */}
+          <div className='flex flex-col items-center h-[100vh] sm:h-auto sm:items-center sm:flex-row sm:justify-around py-10 px-20'>
 
-          <div className='flex flex-col items-center sm:flex-row sm:justify-around py-10 px-20'>
+            {/* User Information Section */}
+            <div className='sm:flex-col sm:relative absolute left-4 h-[100%] gap-5'>
+              <h1 className='sm:text-[50px] ext-[18px]'>{user.name}</h1>
+              <h4 className='sm:text-[20px] ext-[10px]'>Welcome back!</h4>
+              <p className='sm:text-[12px] sm:block hidden'>Upgrade your Account to receive <br/> price and volume alerts on your phone</p>
+            </div>
+            
+            {/* Balance and Details Section */}
+            <div className='sm:flex sm:justify-around h-[30vh] w-[100vw] sm:w-auto sm:h-auto  justify-center gap-2 flex'>
+              <div className='text-[10px] flex flex-col sm:p-10 sm:gap-5 justify-between'>
+                <div>
+                  <h4 className='text-[grey]'>USD BALANCE</h4>
+                  <p>15,008,00</p>
+                </div>
+                <div>
+                  <h4 className='text-[grey]'>USD BALANCE</h4>
+                  <p>15,008,00</p>
+                </div>
+                <div>
+                  <h4 className='text-[grey]'>USD BALANCE</h4>
+                  <p>15,008,00</p>
+                </div>
+                <div>
+                  <button className='bg-[green] px-5 py-1'>Details</button>
+                </div>
+              </div>
 
-          <div className=' flex flex-col  h-[100%]  gap-5'>
-
-            <h1 className='sm:text-[50px]'>{user.name}</h1>
-            <h4 className='sm:text-[20px]'>Welcome back!</h4>
-            <p className='sm:text-[12px]'>Upgrade your Account to recieve <br></br>
-            price and volume alert on your phone</p>
-
-          </div>
-          <div className='text-[10px] flex flex-col  p-10  gap-5 justify-between'>
-            <div className=' '>
-              <h4 className='text-[grey]'>USD BALANCE</h4>
-              <p>15,008,00</p>
+              <div className='text-[10px] flex flex-col sm:p-10 gap-5 justify-between'>
+                <div>
+                  <h4 className='text-[grey]'>USD BALANCE</h4>
+                  <p>15,008,00</p>
+                </div>
+                <div>
+                  <h4 className='text-[grey]'>USD BALANCE</h4>
+                  <p>15,008,00</p>
+                </div>
+                <div>
+                  <h4 className='text-[grey]'>USD BALANCE</h4>
+                  <p>15,008,00</p>
+                </div>
+                <div>
+                  <button className='bg-[green] px-5 py-1'>Details</button>
+                </div>
+              </div>
             </div>
-            <div>
-            <h4 className='text-[grey]'>USD BALANCE</h4>
-            <p>15,008,00</p>
-            </div>
-            <div>
-            <h4 className='text-[grey]'>USD BALANCE</h4>
-            <p>15,008,00</p>
-            </div>
-            <div>
-            <button className='bg-[green] px-5 py-1'>Details</button>
-            </div>
-          </div>
-
-          <div className='text-[10px] flex flex-col  p-10  gap-5 justify-between'>
-            <div className=' '>
-              <h4 className='text-[grey]'>USD BALANCE</h4>
-              <p>15,008,00</p>
-            </div>
-            <div>
-            <h4 className='text-[grey]'>USD BALANCE</h4>
-            <p>15,008,00</p>
-            </div>
-            <div>
-            <h4 className='text-[grey]'>USD BALANCE</h4>
-            <p>15,008,00</p>
-            </div>
-            <div>
-            <button className='bg-[green] px-5 py-1'>Details</button>
-            </div>
-
           
-
+            {/* Chart Section */}
+            <div className='h-[200px] w-[200px] sm:h-[400px] sm:w-[400px]'>
+              <Chart />
+            </div>
           </div>
-          <div className=' h-[400px] w-[400px]'>
-         
-          <Chart/>
-          </div>
-          
-          
-          </div>
-          
-          
-        
         </div>
-
-        
       ) : (
         <p>Loading...</p>
       )}
     </div>
+ 
+      <Profile/>
+    </>
   );
 }
 
---
+++ frontend/votingFrontend/src/components/Profile.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import {useState,useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+function Profile() {
+
+    const navigate = useNavigate()
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        console.log(token)
+        // if (!token) {
+        //   navigate('/login');
+        //   return;
+        // }
+    
+        async function fetchUser() {
+          try {
+            const response = await axios.get('http://localhost:4001/home', {
+              headers: { Authorization: `Bearer ${token}` }
+            });
+            
+            setUser(response.data.users);
+          } catch (error) {
+            setError('Failed to fetch user data');
+            navigate('/login');
+          }
+        }
+    
+        fetchUser();
+      }, []);
+  return (
+    
+    <>
+        <div className='flex flex-col relative bg-[purple] h-[100vh]'>
+
+        <div className='circle absolute top-[35%] w-[100px] h-[100px] bg-black border rounded-full left-[5%]'></div>
+
+        <div className='upper h-[40%] bg-[black] items-center'>
+
+        </div>
+        <div className='lower h-[60%] bg-[#528a52]  '>
+
+            <div className='sm:px-20  text-black flex-col sm:py-14  justify-center items-center '>
+              <h1 className='sm:text-[30px] m-10 sm:m-auto text-[25px] py-5'>{user?.name}</h1>
+              <p className='sm:text-[10px] sm:block hidden w-80 text-[#282626]'>Upgrade your Account to receive  price and volume alerts on your phone
+              Upgrade your Account to receive  price and volume alerts on your phone
+              Upgrade your Account to receive  price and volume alerts on your phone</p>
+           
+            </div>
+            <div className='border text-white flex justify-between'>
+            <div className='flex justify-evenly w-[50%]'>
+            <h4>Profile</h4>
+                <h4>Profile</h4>
+                <h4>Profile</h4>
+                <h4>Profile</h4>
+                <h4>Profile</h4>
+            </div>
+             
+                <div></div>
+            </div>
+
+            
+
+
+         </div>
+
+        </div>
+    </>
+  )
+}
+
+export default Profile
